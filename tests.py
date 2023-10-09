9cf615f86eb8d9b7272bdcf38925e4b9ebe2493b
Dan1l0s <danilos01pr@gmail.com>
2023-10-10 00:15:58 +0300

    Refactored tests

--- tests.py
+++ tests.py
@@ -1,18 +1,21 @@
 import unittest
+from encoder import Encoder
 
 
 class EncoderTests(unittest.TestCase):
+    encoder = Encoder()
+
     def test_caesar_encrypt_typeerror(self):
-        self.assertRaises(TypeError, caesar_encrypt, "Hello world!", "offset!")
+        self.assertRaises(TypeError, self.encoder.caesar_encrypt, "Hello world!", "offset!")
 
     def test_caesar_encrypt_valueerror(self):
-        self.assertRaises(ValueError, caesar_encrypt, "Hello world!", -25)
+        self.assertRaises(ValueError, self.encoder.caesar_encrypt, "Hello world!", -25)
 
     def test_caesar_encrypt_en(self):
-        self.assertEqual(caesar_encrypt("Hello, world!", 2), "Jgnnq, yqtnf!")
+        self.assertEqual(self.encoder.caesar_encrypt("Hello, world!", 2), "Jgnnq, yqtnf!")
 
     def test_caesar_encrypt_ru(self):
-        self.assertEqual(caesar_encrypt("Привет, мир!", 2), "Сткджф, окт!")
+        self.assertEqual(self.encoder.caesar_encrypt("Привет, мир!", 2), "Сткджф, окт!")
 
 
 if __name__ == '__main__':
