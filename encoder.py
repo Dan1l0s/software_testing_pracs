bd33f71bb56d9a313ae83fcc4c7389afee63d089
Dan1l0s <danilos01pr@gmail.com>
2023-10-10 00:15:46 +0300

    Added encoder class, caesar encrypt method

---
+++ encoder.py
@@ -0,0 +1,28 @@
+class Encoder:
+    def caesar_encrypt(self, text: str, offset: int) -> str:
+        if type(offset) != int:
+            raise TypeError('Text must be a string and offset must be an integer')
+        if offset < 1:
+            raise ValueError('Offset must be in range [1, 32]')
+
+        text = str(text)
+        result = ""
+        alphabet_ru_upper = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+        alphabet_ru_lower = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяабвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+        alphabet_en = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+        for symbol in text:
+            if symbol in alphabet_ru_upper:
+                result += alphabet_ru_upper[alphabet_ru_upper.find(symbol) + offset]
+
+            elif symbol in alphabet_ru_lower:
+                result += alphabet_ru_lower[alphabet_ru_lower.find(symbol) + offset]
+
+            elif symbol in alphabet_en:
+                if (symbol.isupper()):
+                    result += chr((ord(symbol) + offset - 65) % 26 + 65)
+                else:
+                    result += chr((ord(symbol) + offset - 97) % 26 + 97)
+            else:
+                result += symbol
+        return result
